fa2957fc6790be3d04ce69780b1ef92186200252
cecile.talec13 <cecile.talec13@gmail.com>
2024-03-15 15:36:09 +0100

    ajout WFS et cluster

--- cartestages.ensg.eu/JS/index.js
+++ cartestages.ensg.eu/JS/index.js
@@ -26,8 +26,8 @@ var PlanOSM = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // Instanciation de la carte
 var map = L.map('map',{
-    center: [47.5603325724831, -2.877608907719759],
-    zoom: 9,
+    center: [48.84109788690544, 2.5872660385494566],
+    zoom: 4,
     layers: [PlanOSM],
     zoomControl :false
 });
@@ -44,3 +44,36 @@ new L.control.zoom({position :'bottomright'}).addTo(map);
 var legend_act = L.control({position: 'bottomright'});
 var legend_vit = L.control({position: 'bottomright'});
 var legend_car = L.control({position: 'bottomright'});
+
+
+// Cluster pour les affichages des zones
+var cluster = new L.MarkerClusterGroup();
+
+// Ajout couche de points WFS
+
+/// Définition de l'URL du service WFS
+var wfsUrl = 'http://172.31.58.191:8080/geoserver/Stages/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=Stages%3Aview_internship&maxFeatures=50&outputFormat=application%2Fjson';
+
+/// Requête au WFS création des points et clusterisation
+fetch(wfsUrl)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        // Boucle sur la réponse
+        data.features.forEach(function(feature) {
+            // Extraction de la latitude et la longitude
+            var latlng = L.latLng(feature.geometry.coordinates[1], feature.geometry.coordinates[0]);
+    
+            // Création d'un marker au point en question, avec une couleur de marker dépendant de l'activité
+            var marker = L.marker(latlng, {icon : redIcon});
+        
+            // Ajout du marker au cluster
+            cluster.addLayer(marker)
+        })
+        // Ajout du cluster sur la carte
+        cluster.addTo(map);
+    })
+    .catch(function(error) {
+        console.error('Error fetching WFS data:', error);
+});
